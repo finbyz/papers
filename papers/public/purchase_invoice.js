c7ec4a1117f592a699ddfc9cc22df9512ab80f99
Finbyz <info@finbyz.tech>
2023-05-26 18:11:13 +0530

    changes is purchase invoice-JV

--- papers/public/purchase_invoice.js
+++ papers/public/purchase_invoice.js
@@ -1,12 +1,20 @@
 
+
 frappe.ui.form.on('Purchase Invoice Item', {
-    calculate:function(frm,cdt,cdn){
-        let doc =locals[cdt][cdn];
-        if(doc.qty){
-           let rate = ((doc.rate_per_kg*doc.total_weight)/doc.qty)
-        frappe.model.set_value(cdt, cdn, 'rate', rate)
-        }
-    },
-    
-});
-    
+	calculate:function(frm,cdt,cdn){
+		let doc =locals[cdt][cdn];
+		if(doc.qty){
+		   let rate = ((doc.rate_per_kg*doc.total_weight)/doc.qty)
+		frappe.model.set_value(cdt, cdn, 'rate', rate)
+		}
+		if(doc.invoice_amount){
+			let rate = ((doc.invoice_amount)/doc.qty)
+			frappe.model.set_value(cdt,cdn,"amount",frm.doc.invoice_amount)
+		frappe.model.set_value(cdt, cdn, 'rate', rate)
+		}
+	},
+   
+	
+}
+);
+	
